f720a9eebbd25b776aac9e8f3be5c6ef43e14a00
houshuo <houshuo3@126.com>
2023-12-07 10:35:06 +0800

    1

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,98 +1,192 @@
+#if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
+#define USE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
- 
-public class CameraController : MonoBehaviour {
- 
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-    
-    // public Camera myCamera;
-    
-    public float mainSpeed = 50.0f; //regular speed
-    public float camSens = 0.15f; //How sensitive it with mouse
-    public float shiftAdd = 100.0f; //multiplied by how long shift is held.  Basically running
-    public float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
-    private float totalRun= 1.0f;
+public class CameraController : MonoBehaviour
+{
+    const float k_MouseSensitivityMultiplier = 0.01f;
 
-    public float rotateSpeed = 1f;
-    public float vertSpeed = .5f;
+    /// <summary>
+    /// Rotation speed when using a controller.
+    /// </summary>
+    public float m_LookSpeedController = 120f;
+    /// <summary>
+    /// Rotation speed when using the mouse.
+    /// </summary>
+    public float m_LookSpeedMouse = 4.0f;
+    /// <summary>
+    /// Movement speed.
+    /// </summary>
+    public float m_MoveSpeed = 10.0f;
+    /// <summary>
+    /// Value added to the speed when incrementing.
+    /// </summary>
+    public float m_MoveSpeedIncrement = 2.5f;
+    /// <summary>
+    /// Scale factor of the turbo mode.
+    /// </summary>
+    public float m_Turbo = 10.0f;
 
+#if !USE_INPUT_SYSTEM
+    private static string kMouseX = "Mouse X";
+    private static string kMouseY = "Mouse Y";
+    private static string kRightStickX = "Controller Right Stick X";
+    private static string kRightStickY = "Controller Right Stick Y";
+    private static string kVertical = "Vertical";
+    private static string kHorizontal = "Horizontal";
 
-    void Update () {
-        lastMouse = Input.mousePosition - lastMouse ;
-        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
-        lastMouse =  Input.mousePosition;
-        //Mouse  camera angle done.  
-       
-        //Keyboard commands
-        // float f = 0.0f;
-        Vector3 p = GetBaseInput();
-        if (p.sqrMagnitude > 0){ // only move while a direction key is pressed
-          if (Input.GetKey (KeyCode.LeftShift)){
-              totalRun += Time.deltaTime;
-              p  = p * totalRun * shiftAdd;
-              p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-              p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-              p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-          } else {
-              totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-              p = p * mainSpeed;
-          }
-         
-          p = p * Time.deltaTime;
-          Vector3 newPosition = transform.position;
-          if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
-              transform.Translate(p);
-              newPosition.x = transform.position.x;
-              newPosition.z = transform.position.z;
-              transform.position = newPosition;
-          } else {
-              transform.Translate(p);
-          }
-        }
+    private static string kYAxis = "YAxis";
+    private static string kSpeedAxis = "Speed Axis";
+#endif
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
-            
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+#if USE_INPUT_SYSTEM
+    InputAction lookAction;
+    InputAction moveAction;
+    InputAction speedAction;
+    InputAction yMoveAction;
+#endif
 
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Rotate(-Vector3.right * rotateSpeed * .75f * Time.deltaTime);
-            
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Rotate(Vector3.right * rotateSpeed * .75f * Time.deltaTime);
-
-        if (Input.GetKey (KeyCode.Q))
-           transform.Translate(Vector3.up * vertSpeed);
-
-        if (Input.GetKey (KeyCode.E))
-           transform.Translate(Vector3.down * vertSpeed);
+    void OnEnable()
+    {
+        RegisterInputs();
     }
-     
-    private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
-        Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey (KeyCode.W)){
-            p_Velocity += new Vector3(0, 0 , 1);
+
+    void RegisterInputs()
+    {
+#if USE_INPUT_SYSTEM
+        var map = new InputActionMap("Free Camera");
+
+        lookAction = map.AddAction("look", binding: "<Mouse>/delta");
+        moveAction = map.AddAction("move", binding: "<Gamepad>/leftStick");
+        speedAction = map.AddAction("speed", binding: "<Gamepad>/dpad");
+        yMoveAction = map.AddAction("yMove");
+
+        lookAction.AddBinding("<Gamepad>/rightStick").WithProcessor("scaleVector2(x=15, y=15)");
+        moveAction.AddCompositeBinding("Dpad")
+            .With("Up", "<Keyboard>/w")
+            .With("Up", "<Keyboard>/upArrow")
+            .With("Down", "<Keyboard>/s")
+            .With("Down", "<Keyboard>/downArrow")
+            .With("Left", "<Keyboard>/a")
+            .With("Left", "<Keyboard>/leftArrow")
+            .With("Right", "<Keyboard>/d")
+            .With("Right", "<Keyboard>/rightArrow");
+        speedAction.AddCompositeBinding("Dpad")
+            .With("Up", "<Keyboard>/home")
+            .With("Down", "<Keyboard>/end");
+        yMoveAction.AddCompositeBinding("Dpad")
+            .With("Up", "<Keyboard>/pageUp")
+            .With("Down", "<Keyboard>/pageDown")
+            .With("Up", "<Keyboard>/e")
+            .With("Down", "<Keyboard>/q")
+            .With("Up", "<Gamepad>/rightshoulder")
+            .With("Down", "<Gamepad>/leftshoulder");
+
+        moveAction.Enable();
+        lookAction.Enable();
+        speedAction.Enable();
+        yMoveAction.Enable();
+#endif
+
+#if UNITY_EDITOR && !USE_INPUT_SYSTEM
+        List<InputManagerEntry> inputEntries = new List<InputManagerEntry>();
+
+        // Add new bindings
+        inputEntries.Add(new InputManagerEntry { name = kRightStickX, kind = InputManagerEntry.Kind.Axis, axis = InputManagerEntry.Axis.Fourth, sensitivity = 1.0f, gravity = 1.0f, deadZone = 0.2f });
+        inputEntries.Add(new InputManagerEntry { name = kRightStickY, kind = InputManagerEntry.Kind.Axis, axis = InputManagerEntry.Axis.Fifth, sensitivity = 1.0f, gravity = 1.0f, deadZone = 0.2f, invert = true });
+
+        inputEntries.Add(new InputManagerEntry { name = kYAxis, kind = InputManagerEntry.Kind.KeyOrButton, btnPositive = "page up", altBtnPositive = "joystick button 5", btnNegative = "page down", altBtnNegative = "joystick button 4", gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
+        inputEntries.Add(new InputManagerEntry { name = kYAxis, kind = InputManagerEntry.Kind.KeyOrButton, btnPositive = "q", btnNegative = "e", gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
+
+        inputEntries.Add(new InputManagerEntry { name = kSpeedAxis, kind = InputManagerEntry.Kind.KeyOrButton, btnPositive = "home", btnNegative = "end", gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
+        inputEntries.Add(new InputManagerEntry { name = kSpeedAxis, kind = InputManagerEntry.Kind.Axis, axis = InputManagerEntry.Axis.Seventh, gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
+
+        InputRegistering.RegisterInputs(inputEntries);
+#endif
+    }
+
+    float inputRotateAxisX, inputRotateAxisY;
+    float inputChangeSpeed;
+    float inputVertical, inputHorizontal, inputYAxis;
+    bool leftShiftBoost, leftShift, fire1;
+
+    void UpdateInputs()
+    {
+        inputRotateAxisX = 0.0f;
+        inputRotateAxisY = 0.0f;
+        leftShiftBoost = false;
+        fire1 = false;
+
+#if USE_INPUT_SYSTEM
+        var lookDelta = lookAction.ReadValue<Vector2>();
+        inputRotateAxisX = lookDelta.x * m_LookSpeedMouse * k_MouseSensitivityMultiplier;
+        inputRotateAxisY = lookDelta.y * m_LookSpeedMouse * k_MouseSensitivityMultiplier;
+
+        leftShift = Keyboard.current?.leftShiftKey?.isPressed ?? false;
+        fire1 = Mouse.current?.leftButton?.isPressed == true || Gamepad.current?.xButton?.isPressed == true;
+
+        inputChangeSpeed = speedAction.ReadValue<Vector2>().y;
+
+        var moveDelta = moveAction.ReadValue<Vector2>();
+        inputVertical = moveDelta.y;
+        inputHorizontal = moveDelta.x;
+        inputYAxis = yMoveAction.ReadValue<Vector2>().y;
+#else
+        if (Input.GetMouseButton(1))
+        {
+            leftShiftBoost = true;
+            inputRotateAxisX = Input.GetAxis(kMouseX) * m_LookSpeedMouse;
+            inputRotateAxisY = Input.GetAxis(kMouseY) * m_LookSpeedMouse;
         }
-        if (Input.GetKey (KeyCode.S)){
-            p_Velocity += new Vector3(0, 0, -1);
+        inputRotateAxisX += (Input.GetAxis(kRightStickX) * m_LookSpeedController * k_MouseSensitivityMultiplier);
+        inputRotateAxisY += (Input.GetAxis(kRightStickY) * m_LookSpeedController * k_MouseSensitivityMultiplier);
+
+        leftShift = Input.GetKey(KeyCode.LeftShift);
+        fire1 = Input.GetAxis("Fire1") > 0.0f;
+
+        inputChangeSpeed = Input.GetAxis(kSpeedAxis);
+
+        inputVertical = Input.GetAxis(kVertical);
+        inputHorizontal = Input.GetAxis(kHorizontal);
+        inputYAxis = Input.GetAxis(kYAxis);
+#endif
+    }
+
+    void Update()
+    {
+        UpdateInputs();
+
+        if (inputChangeSpeed != 0.0f)
+        {
+            m_MoveSpeed += inputChangeSpeed * m_MoveSpeedIncrement;
+            if (m_MoveSpeed < m_MoveSpeedIncrement) m_MoveSpeed = m_MoveSpeedIncrement;
         }
-        if (Input.GetKey (KeyCode.A)){
-            p_Velocity += new Vector3(-1, 0, 0);
+
+        bool moved = inputRotateAxisX != 0.0f || inputRotateAxisY != 0.0f || inputVertical != 0.0f || inputHorizontal != 0.0f || inputYAxis != 0.0f;
+        if (moved)
+        {
+            float rotationX = transform.localEulerAngles.x;
+            float newRotationY = transform.localEulerAngles.y + inputRotateAxisX;
+
+            // Weird clamping code due to weird Euler angle mapping...
+            float newRotationX = (rotationX - inputRotateAxisY);
+            if (rotationX <= 90.0f && newRotationX >= 0.0f)
+                newRotationX = Mathf.Clamp(newRotationX, 0.0f, 90.0f);
+            if (rotationX >= 270.0f)
+                newRotationX = Mathf.Clamp(newRotationX, 270.0f, 360.0f);
+
+            transform.localRotation = Quaternion.Euler(newRotationX, newRotationY, transform.localEulerAngles.z);
+
+            float moveSpeed = Time.deltaTime * m_MoveSpeed;
+            if (fire1 || leftShiftBoost && leftShift)
+                moveSpeed *= m_Turbo;
+            transform.position += transform.forward * moveSpeed * inputVertical;
+            transform.position += transform.right * moveSpeed * inputHorizontal;
+            transform.position += Vector3.up * moveSpeed * inputYAxis;
         }
-        if (Input.GetKey (KeyCode.D)){
-            p_Velocity += new Vector3(1, 0, 0);
-        }
-        return p_Velocity;
     }
 }
+
